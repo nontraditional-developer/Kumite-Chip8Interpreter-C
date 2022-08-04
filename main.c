ae32ca95dda3cd1fc69d7e4819923be91cc4816c
John Rodriguez <johnthedeveloper006@gmail.com>
2022-08-03 20:28:53 -0500

    Changed buffer to char*, but need to iterate thru char* correctly

--- main.c
+++ main.c
@@ -1,18 +1,52 @@
 #include "stdio.h"
+#include "stdlib.h"
 
+// Review: https://stackoverflow.com/questions/48367022/c-iterate-through-char-array-with-a-pointer
+// to iterate thru char* correctly, hopefully 
 
 int main()
 {
     printf("hello\n");
-    unsigned char buffer[10];
+    char* romFile = "Roms/1-ibm-logo.ch8";
     FILE* filePtr;
+    char* romBuffer;    
+    unsigned int fileLen;
 
-    filePtr = fopen("Roms/1-ibm-logo.ch8", "rb");
-    fread(buffer, sizeof(buffer), 1, filePtr);
-
-    for (int i = 0; i < 10; i++) {
-        printf("i: %d, hex: %x\n", i, buffer[i]);
+    filePtr = fopen(romFile, "rb");
+    if (!filePtr) {
+        fprintf(stderr, "Unable to open file");
     }
+    else {
+        //Get file length
+        fseek(filePtr, 0, SEEK_END);
+        fileLen=ftell(filePtr);
+        fseek(filePtr, 0, SEEK_SET);
+    }
+
+    //Allocate memory
+	romBuffer=(char *)malloc(fileLen+1);
+	if (!romBuffer)
+	{
+		fprintf(stderr, "Memory error!");
+        fclose(filePtr);
+	}
+    else {
+        //Read file contents into buffer
+        fread(romBuffer, fileLen, 1, filePtr);
+        fclose(filePtr);
+        printf("fileLen: %d\n", fileLen);
+    }
+
+    // DO WHATEVER W/ BUFFER
+    for (int i = 0; i < fileLen; i++) {
+        printf("i: %d, hex: %x\n", i, romBuffer[i]);
+    }
+
+    // free allocated memory
+    free(romBuffer);
+
+
+    
     
 
     return 0;
